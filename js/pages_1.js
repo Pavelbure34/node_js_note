582e7b4c50f48bcf5d4c6ac53c59938597d21266
Dongbin Suh <dongbinsuh@gmail.com>
2019-06-28 20:33:20 +0900

    fundamental ch2 init

--- js/pages_1.js
+++ js/pages_1.js
@@ -1,6 +1,40 @@
 const fundamental = {
     ch01:
-        '',
+        '<h2>What is Javascript and Node.js?</h2>\
+        <table>\
+            <tr>\
+                <th>Javascript</th>\
+                <th>Node.js</th>\
+            </tr>\
+            <tr>\
+                <td>\
+                    a programming language usually<br>\
+                    running on web browser.\
+                </td>\
+                <td>\
+                    Javascript Runtime\
+                </td>\
+            </tr>\
+        </table>'+
+        '<section>\
+            <h3>What do they do?</h3>\
+            <ul class="noBulletPt">\
+                <caption>Javascript</caption>\
+                <li>allows dynamic web functions.</li>\
+            </ul>\
+            <ul class="noBulletPt">\
+                <caption>Node.js</caption>\
+                <li>is a javascript library that\
+                    <emR>allows javascript code run on the server</emR>\
+                </li>\
+            </ul>\
+        </section>'+
+        '<section>\
+            <h3>How?</h3>\
+            Javascript code is run on V8 engine which C++ based engine compiles\
+            js code in your browser into machine code.<br>\
+            Node.js makes a shortcut to that!\
+        </section>',
     ch02:
         ''
 }
